373f1c2b73a31d1a4c4f0fa162abd72a3a6bb130
hmalavia <hmalavia2@gmail.com>
2025-03-03 19:56:50 +0530

    v0.0.2

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 - Add images to a new dir called images.
 - Update system_prompt.txt as per your desire.
 - Run the notebook.
+### v0.0.1 release notes:
+- Generate/Regenerate caption for single/multiple images
+- Modular langchain framework
+- Tested with gpt-4o-mini
+- Notebook interface only
+- Single image context
+- Not saving on disk
+- Only reads .jpg/.JPG files
+### v0.0.2 (current) release notes :
+- Added save to disk function (Done)
+- Updated to use multi image prompts and to improve generation variety along with updated default system prompt.
--- utils.py
+++ utils.py
@@ -5,12 +5,13 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import base64
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser,JsonOutputParser
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from PIL import Image
+import json
 
 def filesize_gt_20mb(image_path):
     filesize = os.stat(image_path).st_size/1024**2
@@ -33,31 +34,69 @@ def encode_image(image_path):
 def get_file_names(image_files_dir):
     return [file for file in os.listdir('images') if (file.endswith('.jpg') or file.endswith('.JPG')) and not file.endswith('_resized.jpg')]
 
-def genrate_caption(chain,system_prompt,image_file_name):
-    image_path = f'images/{image_file_name}'
-    if filesize_gt_20mb(image_path=image_path):
-        base64_image = encode_image(image_path=compress_image_opencv(input_path=image_path,quality=85))
-        response = chain.invoke({'system_prompt':system_prompt,'base64_image':base64_image})
-        return {image_file_name:response}
-    else:
-        base64_image = encode_image(image_path=image_path)
-        response = chain.invoke({'system_prompt':system_prompt,'base64_image':base64_image})
-        return {image_file_name:response}
-    
-def display_results(final_results_dict):
-    for k,v in final_results_dict.items():
+def save_output(final_result_dict):
+    with open('final_output.json', 'w') as fp:
+        json.dump(final_result_dict, fp,indent=4)
+    print('Output Saved')
+
+def display_results(results_dict):
+    for k,v in results_dict.items():
         im = Image.open(f'images/{k}')
         plt.imshow(im)
         plt.show()
         print(v)
 
+def gen_image_encoding(image_file_name):
+    image_path = f'images/{image_file_name}'
+    if filesize_gt_20mb(image_path=image_path):
+        base64_image = encode_image(image_path=compress_image_opencv(input_path=image_path,quality=85))
+    else:
+        base64_image = encode_image(image_path=image_path)
+    return base64_image
+
+def generate_sytem_prompt(sys_prompt_file):
+    with open(sys_prompt_file,'r') as prompt_file:
+        base_system_prompt = prompt_file.read()
+        output_schema_prompt = """\nYou should return the output wrapped in JSON tags using the schema given below
+                            {image file name (provided with the image input): 
+                            your generated caption as per system instructions }"""
+        system_prompt = base_system_prompt + output_schema_prompt
+    print(f"System Prompt is: \n {base_system_prompt} \n")
+    return SystemMessage(content=system_prompt)
+
+def generate_user_prompt(image_list):
+    user_prompt_list = []
+    for i in image_list:
+        base64_image = gen_image_encoding(image_file_name=i)
+        user_prompt_list.extend([{
+            "type":"text",
+            "text":f"Image file name:{i}"
+        },
+        {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}
+        }])
+    return HumanMessage(content=user_prompt_list)
+
+def generate_final_prompt(system_prompt_file,image_list):
+    system_message = generate_sytem_prompt(sys_prompt_file=system_prompt_file)
+    user_message = generate_user_prompt(image_list=image_list)
+    print(f'Num of Images: {len(image_list)}')
+    return (system_message + user_message)
+
+def genrate_caption(llm,system_prompt_file,image_list):
+    prompt = generate_final_prompt(system_prompt_file=system_prompt_file,image_list=image_list)
+    parser = JsonOutputParser()
+    chain = prompt | llm | parser
+    res_dict = chain.invoke({})
+    display_results(res_dict)
+    return res_dict
+    
 def regenrate_caption(chain,system_prompt,image_file_name,initial_output):
     image_path = f'images/{image_file_name}'
     if filesize_gt_20mb(image_path=image_path):
         base64_image = encode_image(image_path=compress_image_opencv(input_path=image_path,quality=85))
-        response = chain.invoke({'system_prompt':system_prompt,'initial_output':initial_output,'base64_image':base64_image})
-        return {image_file_name:response}
+        return(chain.invoke({'system_prompt':system_prompt,'image_file_name':image_file_name,'initial_output':initial_output,'base64_image':base64_image}))
     else:
         base64_image = encode_image(image_path=image_path)
-        response = chain.invoke({'system_prompt':system_prompt,'initial_output':initial_output,'base64_image':base64_image})
-        return {image_file_name:response}
+        return(chain.invoke({'system_prompt':system_prompt,'image_file_name':image_file_name,'initial_output':initial_output,'base64_image':base64_image}))
